714f89774fb3547a936aa0c1d6b1e7cc51c1ee00
Lewis Van den Cruyce <lewis.vandencruyce@gmail.com>
2023-08-27 00:41:34 +0200

    add transition and change grid refresh

--- index.js
+++ index.js
@@ -5,20 +5,29 @@ for (var gridNumber = 0; gridNumber < 100; gridNumber++) {
 
 state[0] = "possible";
 
+var changeStatus = (gridNumber, oldClass, newClass) => {
+  var clickedGrid = $(`[data-gridNumber="${gridNumber}"]`);
+  clickedGrid.removeClass(oldClass);
+  clickedGrid.addClass(newClass);
+
+  state[gridNumber] = newClass;
+};
+
 var setPossible = (x, y) => {
   if (0 <= x && x <= 9 && 0 <= y && y <= 9 && state[x * 10 + y] === "empty") {
-    state[x * 10 + y] = "possible";
+    changeStatus(x * 10 + y, "empty", "possible");
   }
 };
 
 var handleClick = (gridNumber) => {
   if (state[gridNumber] === "possible") {
     // fill the cell
-    state[gridNumber] = "filled";
+    changeStatus(gridNumber, "possible", "filled");
+
     // remove all previous possibles
     for (var x = 0; x < 100; x++) {
       if (state[x] === "possible") {
-        state[x] = "empty";
+        changeStatus(x, "possible", "empty");
       }
     }
 
@@ -35,7 +44,7 @@ var handleClick = (gridNumber) => {
     setPossible(x, y - 3);
     setPossible(x + 2, y - 2);
   }
-  createGrid(10);
+  //createGrid(10);
 };
 
 // function that builds a grid in the "container"
@@ -45,11 +54,7 @@ function createGrid(x) {
     for (var columns = 0; columns < x; columns++) {
       var gridNumber = columns + rows * x;
       $("#container").append(
-        "<div class='grid type" +
-          state[gridNumber] +
-          "' onclick='handleClick(" +
-          gridNumber +
-          ")'></div>"
+        `<div data-gridNumber='${gridNumber}' class='grid ${state[gridNumber]}' onclick='handleClick(${gridNumber})'></div>`
       );
     }
   }
